452a818416152c8f8044940c5c4cd91e3cabd633
ABoni14 <aboni150@gmail.com>
2022-01-17 13:03:52 +0100

    last commit

---
+++ database.php
@@ -0,0 +1,25 @@
+<?php
+
+$movies = [
+  [
+    "title"=>"Il signore degli anelli",
+    "date"=>"2001",
+    "vote"=>"10",
+    "genre"=>"Fantascienza",
+    "description"=>"Uno Hobbit trova un anello potentissimo e dovrà affrontare un lungo viaggio."
+  ],
+  [
+    "title"=>"Shining",
+    "date"=>"1980",
+    "vote"=>"9",
+    "genre"=>"Horror",
+    "description"=>"Uno famiglia si trasferisce come custode in un Hotel isolato e la solitudine gioca brutti scherzi al padre di famiglia."
+  ],
+  [
+    "title"=>"Don't Look Up",
+    "date"=>"2022",
+    "vote"=>"8",
+    "genre"=>"Apocalittico",
+    "description"=>"Un meteorite ricco di materiali prezioni sta per distruggere la Terra ma ai piani alti interessa solo il guadagno per la missione si salvataggio e non la vita umana."
+  ],
+];
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
-
+require_once __DIR__ . "/Movie.php";
+require_once __DIR__ . "/database.php";
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +10,23 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=
   , initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Movie</title>
 </head>
 <body>
-  
+<div class="container">
+  <?php foreach($movies as $movie) : 
+  $new_movie = new Movie($movie["title"], $movie["date"], $movie["vote"], $movie["genre"], $movie["description"])
+  ?>
+  <div class="card">
+    <h3><?php echo $new_movie->title ?></h3>
+    <h5><?php echo $new_movie->date ?></h5>
+    <h4>Voto: <?php echo $new_movie->vote ?></h4>
+    <h4><?php echo $new_movie->genre ?></h4>
+    <p>Descrizione: <?php echo $new_movie->getExcept() ?></p>
+  </div>
+  <?php endforeach; ?>
+</div>
+
 </body>
 </html>
